2779cc4bd1fa18d3fb84d87a397afd48a64bf6fb
BouguesriRania <hr_bouguesri@esi.dz>
2021-04-30 17:05:39 +0100

    final

--- app/src/main/java/com/example/ran/mybio/Main2Activity.kt
+++ app/src/main/java/com/example/ran/mybio/Main2Activity.kt
@@ -1,63 +1,36 @@
 package com.example.ran.mybio
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
+import kotlin.*
 
 class Main2Activity : AppCompatActivity() {
     val random = Random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    var i=rand(0,3)
-        if (i==0) {
-            val Image = findViewById(R.id.imageView) as ImageView
-            val drawble = resources.getDrawable(R.drawable.barack,theme)
-            Image.setImageDrawable(drawble)
-            val text= findViewById<TextView>(R.id.textView2)
-            val string = resources.getString(R.string.text)
-            text.setText(string)
-            val text2= findViewById<TextView>(R.id.textView)
-            val string2 = resources.getString(R.string.title)
-            text2.setText(string2)
-        }
-        if (i==1) {
-            val Image = findViewById(R.id.imageView) as ImageView
-            val drawble = resources.getDrawable(R.drawable.amir,theme)
-            Image.setImageDrawable(drawble)
-            val text= findViewById<TextView>(R.id.textView2)
-            val string = resources.getString(R.string.text2)
-            text.setText(string)
-            val text2= findViewById<TextView>(R.id.textView)
-            val string2 = resources.getString(R.string.title2)
-            text2.setText(string2)
-        }
-        if (i==2) {
-            val Image = findViewById(R.id.imageView) as ImageView
-            val drawble = resources.getDrawable(R.drawable.j,theme)
-            Image.setImageDrawable(drawble)
-            val text= findViewById<TextView>(R.id.textView2)
-            val string = resources.getString(R.string.text3)
-            text.setText(string)
-            val text2= findViewById<TextView>(R.id.textView)
-            val string2 = resources.getString(R.string.title3)
-            text2.setText(string2)
-        }
-        if (i==3) {
-            val Image = findViewById(R.id.imageView) as ImageView
-            val drawble = resources.getDrawable(R.drawable.elif,theme)
-            Image.setImageDrawable(drawble)
-            val text= findViewById<TextView>(R.id.textView2)
-            val string = resources.getString(R.string.text4)
-            text.setText(string)
-            val text2= findViewById<TextView>(R.id.textView)
-            val string2 = resources.getString(R.string.title4)
-            text2.setText(string2)
-        }
+        val Image = findViewById(R.id.imageView) as ImageView
+        val text= findViewById<TextView>(R.id.textView2)
+        val text2= findViewById<TextView>(R.id.textView)
+        val draw_array =arrayOf<Int>(R.drawable.barack,R.drawable.amir,R.drawable.j,R.drawable.elif)
+        val tab = resources.getStringArray(R.array.title_tab)
+        val tab_bio = resources.getStringArray(R.array.bio_tab)
+        var i=rand(0,3)
+
+        val drawble = resources.getDrawable(draw_array[i],theme)
+        Image.setImageDrawable(drawble)
+        val string = tab_bio[i]
+        text.setText(string)
+        val string2 = tab[i]
+        text2.setText(string2)
+
+
 
 
 }
